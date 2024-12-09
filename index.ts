67881ff3cae95c105daf125ffc3f3df596ada906
Umarova Albina <albinaumarova2014@gmail.com>
2024-12-09 19:04:05 +0600

    Update

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import productsRouter from "./routers/products";
 import fileDb from "./fileDb";
+import cors from "cors";
 
 import fs = require("fs");
 
@@ -8,8 +9,8 @@ import fs = require("fs");
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
-
 app.use('/products', productsRouter);
 
 const run = async () => {
